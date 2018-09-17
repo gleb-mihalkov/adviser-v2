7c9cea40aa93c415b60b2e76b4c95ab114b0055a
gleb.mihalkov <gmihalkov123@gmail.com>
2018-09-17 13:19:20 +0300

    Initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+/**
+ * Main view of application.
+ * @returns {JSX.Element} Element.
+ */
+export default function App() {
+
+  return (
+    <Fragment />
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import { createHashHistory } from 'history';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import {
+  applyMiddleware,
+  createStore
+} from 'redux';
+import {
+  ConnectedRouter,
+  connectRouter,
+  routerMiddleware
+} from 'connected-react-router';
+
+import App from './App/';
+import registerServiceWorker from './registerServiceWorker';
+
+const history = createHashHistory();
+
+const store = createStore(
+  connectRouter(history)(state => state),
+  applyMiddleware(
+    routerMiddleware(history)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Route path="/" component={App} />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
