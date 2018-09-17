084a7143ce89894e9c4474aeb3bc4504d19defe8
gleb.mihalkov <gmihalkov123@gmail.com>
2018-09-17 13:50:30 +0300

    Add prop which controls the visibility of AppLayout_Aside

--- src/App/AppLayout/AppLayout.jsx
+++ src/App/AppLayout/AppLayout.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import classnames from 'classnames';
 import './AppLayout.scss';
 
 /**
  * Layout of application.
- * @param {JSX.Element} aside    Aside content of application.
- * @param {JSX.Element} children Main content of application.
+ * @param {Boolean}     isAsideShowed True if aside must be showed
+ *                                    on mobile device.
+ * @param {JSX.Element} aside         Aside content of application.
+ * @param {JSX.Element} children      Main content of application.
  */
-export default function AppLayout({ aside, children }) {
+export default function AppLayout({ isAsideShowed, aside, children }) {
+  const classes = classnames({
+    'AppLayout': true,
+    'AppLayout-AsideShowed': isAsideShowed
+  });
 
   return (
-    <div className="AppLayout">
+    <div className={classes}>
       <div className="AppLayout_Main">
         {aside}
       </div>
