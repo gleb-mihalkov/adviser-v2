e14443dded43aa3ed93a46406ccffa512cee7fde
gleb.mihalkov <gmihalkov123@gmail.com>
2018-09-18 12:30:22 +0300

    Replacing styles packages in bundler

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,6 @@
-const rewireSass = require('react-app-rewire-scss');
 const rewireCssModules = require('react-app-rewire-css-modules');
 
-/* config-overrides.js */
 module.exports = function override(config, env) {
   config = rewireCssModules(config, env);
-  config = rewireSass(config, env);
   return config;
 }
