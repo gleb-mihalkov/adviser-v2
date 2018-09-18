d48b434c45dfb57d426d2fd9797fc9de993adf9b
gleb.mihalkov <gmihalkov123@gmail.com>
2018-09-18 12:27:50 +0300

    Add css modules package

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
 const rewireSass = require('react-app-rewire-scss');
+const rewireCssModules = require('react-app-rewire-css-modules');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
+  config = rewireCssModules(config, env);
   config = rewireSass(config, env);
-  // with loaderOptions
-  // config = rewireSass.withLoaderOptions(someLoaderOptions)(config, env);
   return config;
 }
